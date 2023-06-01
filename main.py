8d9b0c65cce5cdaf1d8726d405688f5cd1dd9e21
dimakuchinskyi <kuci.ia77@gmail.com>
2023-06-01 19:51:33 +0300

    #4

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-sum = 0
-for number in range(1, 101):
-    if number % 2 == 0:
-        sum += number
-print("Сума парних чисел від 1 до 100: ", sum)
+ryadok = input("Введіть рядок: ")
+for letter in ryadok:
+    print(letter)
