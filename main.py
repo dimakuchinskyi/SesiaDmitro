cc4bcf046e80b09f00bc46b9ff7437485086755a
dimakuchinskyi <kuci.ia77@gmail.com>
2023-06-01 20:39:12 +0300

    #11

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    def get_info(self):
-        print("Ім'я:", self.name)
-        print("Вік:", self.age)
-student1 = Student("Глеп", 20)
-student1.get_info()
+class BankAccount:
+    def __init__(self, account_number, balance=0):
+        self.account_number = account_number
+        self.balance = balance
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Депозит {amount} грн. здійснено успішно.")
+        else:
+            print("Сума депозиту повинна бути більшою за 0.")
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            print(f"Знято {amount} грн. з рахунку.")
+account = BankAccount("123456789", 1000)
+print(f"Номер рахунку: {account.account_number}")
+print(f"Поточний баланс: {account.balance} грн.")
+account.deposit(500)
+print(f"Поточний баланс: {account.balance} грн.")
+account.withdraw(200)
+print(f"Поточний баланс: {account.balance} грн.")
+account.withdraw(1500)
