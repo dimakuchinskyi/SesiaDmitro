9533b29f5da21b47cab7aa9e0ea656c1522ac448
dimakuchinskyi <kuci.ia77@gmail.com>
2023-06-01 19:46:39 +0300

    #2

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-#1
-print('Привіт, світ!')
+start = int(input("Введіть початкове число: "))
+end = int(input("Введіть кінцеве число: "))
+sum_of_range = 0
+for num in range(start + 1, end):
+    sum_of_range += num
+print("Сума діапазону чисел:", sum_of_range)
