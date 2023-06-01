aec771bd128ed1abd85168fe0dca46834353857f
dimakuchinskyi <kuci.ia77@gmail.com>
2023-06-01 20:41:46 +0300

    #12

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-class BankAccount:
-    def __init__(self, account_number, balance=0):
-        self.account_number = account_number
-        self.balance = balance
-    def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
-            print(f"Депозит {amount} грн. здійснено успішно.")
-        else:
-            print("Сума депозиту повинна бути більшою за 0.")
-    def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
-            print(f"Знято {amount} грн. з рахунку.")
-account = BankAccount("123456789", 1000)
-print(f"Номер рахунку: {account.account_number}")
-print(f"Поточний баланс: {account.balance} грн.")
-account.deposit(500)
-print(f"Поточний баланс: {account.balance} грн.")
-account.withdraw(200)
-print(f"Поточний баланс: {account.balance} грн.")
-account.withdraw(1500)
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    def get_info(self):
+        print("Назва: ", self.title)
+        print("Автор: ", self.author)
+book1 = Book("Війна і мир", "Лев Толстой")
+book1.get_info()
+book2 = Book("Гра Престолів", "Джордж Мартін")
+book2.get_info()
