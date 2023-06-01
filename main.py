b694e0dfc442467963c7280ea9ad94fb02ce6d0e
dimakuchinskyi <kuci.ia77@gmail.com>
2023-06-01 20:30:19 +0300

    #8

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-student_grades = {}
-def create_student_grades():
-    while True:
-        student_name = input("Введіть ім'я студента (або введіть 'кінець' для завершення рахування студентів): ")
-        if student_name == 'кінець':
-            break
-        grade = input("Введіть оцінку студента {}: ".format(student_name))
-        student_grades[student_name] = int(grade)
-def find_student_with_highest_grade():
-    highest_grade = max(student_grades.values())
-    for student, grade in student_grades.items():
-        if grade == highest_grade:
-            return student
-create_student_grades()
-student_with_highest_grade = find_student_with_highest_grade()
-print("Студент з найвищою оцінкою: ", student_with_highest_grade)
+def filter_capitalized_strings(strings):
+    filtered_strings = []
+    for string in strings:
+        if string[0].isupper():
+            filtered_strings.append(string)
+    return filtered_strings
+user_input = input("Введіть рядки, розділені пробілами: ")
+input_list = user_input.split()
+filtered_list = filter_capitalized_strings(input_list)
+print("Результат:")
+for string in filtered_list:
+    print(string)
