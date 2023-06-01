6db950192dbb66087cc56f7ac8987b115082470e
dimakuchinskyi <kuci.ia77@gmail.com>
2023-06-01 20:23:07 +0300

    #7

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-parni = []
-for i in numbers:
-    if i % 2 == 0:
-        parni.append(i)
-print('Список парних чисел:', parni)
+student_grades = {}
+def create_student_grades():
+    while True:
+        student_name = input("Введіть ім'я студента (або введіть 'кінець' для завершення рахування студентів): ")
+        if student_name == 'кінець':
+            break
+        grade = input("Введіть оцінку студента {}: ".format(student_name))
+        student_grades[student_name] = int(grade)
+def find_student_with_highest_grade():
+    highest_grade = max(student_grades.values())
+    for student, grade in student_grades.items():
+        if grade == highest_grade:
+            return student
+create_student_grades()
+student_with_highest_grade = find_student_with_highest_grade()
+print("Студент з найвищою оцінкою: ", student_with_highest_grade)
